056631c400239ae76dcb1b37d9e02e652e97328f
Zach Saffrin <6954752+zsaffrin@users.noreply.github.com>
2024-06-23 16:34:35 -0400

    Add script for package installs

---
+++ scripts/install-shell-packages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Colors
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# ======= zsh =======
+if [[ $(which zsh) = "zsh not found" ]]; then
+  echo "Installing zsh..."
+
+  echo "sudo apt update"
+  echo "sudo apt install zsh -y"
+  
+  echo "${GREEN}${NC} Done"
+else
+  echo "zsh already installed"
+fi
+
+# ======= eza =======
+if [[ $(which eza) = "eza not found" ]]; then
+  echo "Installing eza..."
+
+  echo "sudo mkdir -p /etc/apt/keyrings"
+  echo "wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg"
+  echo "echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list"
+  echo "sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list"
+  echo "sudo apt update"
+  echo "sudo apt install -y eza"
+
+  echo "${GREEN}${NC} Done"
+else
+  echo "eza already installed"
+fi
+
+# ======= oh-my-posh =======
+if [[ $(which oh-my-posh) = "oh-my-posh not found" ]]; then
+  echo "Installing oh-my-posh..."
+
+  echo "sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh"
+  echo "sudo chmod +x /usr/local/bin/oh-my-posh"
+  
+  echo "${GREEN}${NC} Done"
+else
+  echo "oh-my-posh already installed"
+fi
